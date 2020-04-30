4a037a1dd571c7ba1c9cf18ff644e2a596655595
Riazone <52306083+riazone@users.noreply.github.com>
2020-04-29 21:43:41 -0400

    Add files via upload, TypeError: 'NoneType'

---
+++ test/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='Gold-Return-Prediction',
+	version='',
+	packages=[''],
+	package_dir={'': 'test'},
+	url='',
+	license='',
+	author='andrewschell',
+	author_email='andrewscience304@gmail.com',
+	description='My implementation of commodity prices',
+)
