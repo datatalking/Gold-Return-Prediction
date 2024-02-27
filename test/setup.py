efbb475ca0972e78bbafd131d6ae678e389dd1ba
Andrew <paintstone@gmail.com>
2024-02-26 17:12:55 -0800

    Ticker List.csv has 17 elements, its expecting 23, TODO find the missing 5

--- test/setup.py
+++ test/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-	name='Gold-Return-Prediction',
-	version='',
-	packages=[''],
-	package_dir={'': 'test'},
-	url='',
-	license='',
-	author='andrewschell',
-	author_email='andrewscience304@gmail.com',
-	description='My implementation of commodity prices',
+    name='Gold-Return-Prediction',
+    version='',
+    packages=[''],
+    package_dir={'': 'test'},
+    url='',
+    license='',
+    author='andrewschell',
+    author_email='andrewscience304@gmail.com',
+    description='My implementation of commodity prices',
 )
